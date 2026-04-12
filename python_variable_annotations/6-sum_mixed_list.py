fa66cf840ceb5aa79cba4932fe242e37cb73d320
Mohammed <mohammed.secure@outlook.sa>
2026-04-12 22:11:37 +0300

    feat: complete task 6 - mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for Task 6: Complex types - mixed list. """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Returns the sum of a list of integers and floats as a float. """
+    return float(sum(mxd_lst))
