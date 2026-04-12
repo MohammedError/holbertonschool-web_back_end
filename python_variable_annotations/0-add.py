329e47777b5502883544465c75a13e0cb7a3928d
Mohammed <mohammed.secure@outlook.sa>
2026-04-12 22:04:18 +0300

    Initial commit: Completed Task 0 and Task 1

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotations for addition and concatenation. """
+
+
+def add(a: float, b: float) -> float:
+    """ Returns the sum of two floats as a float. """
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic annotations for string concatenation. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns a concatenated string of two strings. """
+    return str1 + str2
