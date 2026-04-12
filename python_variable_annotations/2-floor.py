52f09d05ecea3439b26c2ba7d5eca7a5108cef36
Mohammed <mohammed.secure@outlook.sa>
2026-04-12 22:04:55 +0300

    feat: complete tasks 2 and 3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for Task 2: Basic annotations - floor. """
+import math
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of a float as an int. """
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module for Task 3: Basic annotations - to_str. """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of a float. """
+    return str(n)
