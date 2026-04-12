d4be48a466ef00db1df5a3f4dc0dae06cacb4c3c
Mohammed <mohammed.secure@outlook.sa>
2026-04-12 22:17:03 +0300

    feat: complete task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Module for Task 9: Duck type an iterable. """
+"""Task 9 module"""
 from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(elts: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """ Returns a list of tuples containing elements and their lengths. """
-    return [(i, len(i)) for i in elts]
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns elements and their lengths"""
+    return [(i, len(i)) for i in lst]
