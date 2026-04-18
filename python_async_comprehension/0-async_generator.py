73cf25d1230354bb1984f696f81ca69af670c015
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 22:32:01 +0300

    fix: task 0 type annotation + doc

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""Async generator."""
+"""Asynchronous generator that yields random numbers."""
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield random float numbers between 0 and 10 every second."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
