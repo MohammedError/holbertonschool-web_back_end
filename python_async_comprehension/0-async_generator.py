f38f93f4a0e46db387a64df8c430fc34e171d5e0
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 22:39:50 +0300

    fix: final strict compliance

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous generator."""
+"""This module provides an asynchronous generator."""
 
 import asyncio
 import random
@@ -7,7 +7,8 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Asynchronous generator that yields random numbers."""
-    for i in range(10):
+    """This asynchronous generator yields random float values between 0 and 10."""
+    for index in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
