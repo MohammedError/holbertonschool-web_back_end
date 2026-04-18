3d2b856b2349e51996d23f5d4d0c40775c634045
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:47:34 +0300

    fix: precise pycodestyle and task 0 requirements

--- 0-async_generator.py
+++ 0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Module that contains an asynchronous generator coroutine."""
+"""Async generator module."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Coroutine that loops 10 times, waits 1 second, and yields a random float."""
-    for _ in range(10):
+    """Generates 10 random numbers asynchronously."""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
