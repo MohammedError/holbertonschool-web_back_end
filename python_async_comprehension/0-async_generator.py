8017ba049ba29be24a28d41da9bbde6acd21471e
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:34:51 +0300

    fix: adjust generator output format and AST structure

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
-"""
-This module contains an asynchronous coroutine called async_generator.
-It takes no arguments and yields random float numbers.
-"""
+"""Async Generator Module"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    The coroutine will loop 10 times, each time asynchronously
-    wait 1 second, then yield a random number between 0 and 10.
-    """
-    for _ in range(10):
+    """Yields a random number between 0 and 10, ten times"""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
