050511eb39b9376d1f914e8c3990d7d22b63ceac
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 22:41:23 +0300

    fix: correct generator typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""This module provides an asynchronous generator."""
+"""Module for asynchronous generator."""
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """This asynchronous generator yields random float values between 0 and 10."""
-    for index in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """Generate random float numbers between 0 and 10 asynchronously."""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
 
