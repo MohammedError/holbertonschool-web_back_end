dea6ae08d10be0eef134b41e60c912b414f1f132
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:52:02 +0300

    fix: move 0-async_generator.py into the correct directory

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Module that contains an asynchronous generator coroutine."""
+"""Async Generator Module"""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Coroutine that loops 10 times, waits 1 second, and yields a random float."""
-    for _ in range(10):
+    """Yields a random number between 0 and 10, ten times"""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
