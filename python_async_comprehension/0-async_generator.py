266aa80de744270c0fe3abe2ab993a3a2bf1aca1
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:31:14 +0300

    fix: perfect docstring length and type hint for task 0 check 8

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Async generator module."""
+"""
+This module contains an asynchronous coroutine called async_generator.
+It takes no arguments and yields random float numbers.
+"""
 import asyncio
 import random
 from typing import AsyncGenerator
@@ -7,7 +10,8 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Coroutine that yields 10 random numbers.
+    The coroutine will loop 10 times, each time asynchronously
+    wait 1 second, then yield a random number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
