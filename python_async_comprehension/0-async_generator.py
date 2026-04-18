462d20347a6d27c01ed0157c5f4d8e88fe69e620
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:09:46 +0300

    fix: minimalist ALX standard code for check 8

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-"""
-This module contains an asynchronous generator coroutine that loops
-and yields random numbers based on the project requirements.
-"""
+"""This module provides an asynchronous generator."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    The coroutine will loop 10 times, each time asynchronously wait 1
-    second, then yield a random number between 0 and 10.
-    """
-    for _ in range(10):
+    """This coroutine loops 10 times and yields a random float."""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
