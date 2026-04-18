5ac5790f11fa3831d7fc2ca45cbc6974887685fe
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:00:18 +0300

    fix: explicit typing and strict LF line endings for check 8

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""Module that contains an asynchronous generator coroutine."""
+"""Module for async generator coroutine."""
 import asyncio
 import random
-import typing
+from typing import AsyncGenerator
 
 
-async def async_generator() -> typing.AsyncGenerator[float, None]:
-    """Loops 10 times, waits 1 second, and yields a random number."""
-    for i in range(10):
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Loops 10 times, waits 1 second asynchronously each time,
+    then yields a random float between 0 and 10.
+    """
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
