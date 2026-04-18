52753ac7ee69f334ed46323095ec9f1ca542360a
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:12:33 +0300

    fix: manual pure linux file creation for check 3 and 8

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""This module provides an asynchronous generator."""
+"""Async generator module."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """This coroutine loops 10 times and yields a random float."""
+    """Coroutine that yields 10 random numbers."""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
