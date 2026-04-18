110f3234f8eefe03155c52b3c4deed357316833c
Mohammed <mohammed.secure@outlook.sa>
2026-04-18 23:48:30 +0300

    fix: precise AST compliance and strict type hint for check 8

--- 0-async_generator.py
+++ 0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async generator module."""
+"""Async Generator Module."""
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Generates 10 random numbers asynchronously."""
+    """Loops 10 times, waits 1 sec, and yields a random float."""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
