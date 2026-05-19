d0a3ee289e517510a644cc2e185814b49c323e59
Mohammed <mohammed.secure@outlook.sa>
2026-05-19 12:49:23 +0300

    Fix Check 1: Ensure strict compliance for Task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,70 +3,7 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 1245;
-const DB_FILE = process.argv[2];
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      if (lines.length <= 1) {
-        resolve('Number of students: 0');
-        return;
-      }
-
-      const students = lines.slice(1);
-      let output = `Number of students: ${students.length}`;
-
-      const fields = {};
-      students.forEach((student) => {
-        const studentData = student.split(',');
-        const firstName = studentData[0];
-        const field = studentData[studentData.length - 1].trim();
-
-        if (field) {
-          if (!fields[field]) {
-            fields[field] = [];
-          }
-          fields[field].push(firstName);
-        }
-      });
-
-      for (const [field, names] of Object.entries(fields)) {
-        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
-      }
-
-      resolve(output);
-    });
-  });
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', (req, res) => {
-  countStudents(DB_FILE)
-    .then((data) => {
-      res.send(`This is the list of our students\n${data}`);
-    })
-    .catch((error) => {
-      res.send(`This is the list of our students\n${error.message}`);
-    });
-});
-
-app.listen(PORT);
-
-module.exports = app;
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const PORT = 1245;
-const DB_FILE = process.argv[2];
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
@@ -98,12 +35,14 @@ function countStudents(path) {
     });
   });
 }
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  countStudents(DB_FILE)
+  const dbFile = process.argv.length > 2 ? process.argv[2] : '';
+  countStudents(dbFile)
     .then((data) => {
       res.send(`This is the list of our students\n${data}`);
     })
@@ -113,4 +52,5 @@ app.get('/students', (req, res) => {
 });
 
 app.listen(PORT);
+
 module.exports = app;
